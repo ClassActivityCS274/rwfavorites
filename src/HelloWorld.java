d2d0e1f27fdc1c0881600ad3af54579de4901af0
Richard Wang <richardbdgbcs@gmail.com>
2021-09-13 11:22:18 -0600

    Add method printStuff()

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,10 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("I love GitHub!");
+		System.out.println(printStuff());
+	}
+	
+	public static String printStuff() {
+		return "Richard's Branch";
 	}
 }
